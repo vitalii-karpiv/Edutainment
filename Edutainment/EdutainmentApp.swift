7bed2e13984b9831d23859089868056d73c68330
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-13 21:56:04 +0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by newbie on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
